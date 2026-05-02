e73260030fe9d9199cddc4625ac03d30031475fc
Cumali Bilgic <250541008@firat.edu.tr>
2026-05-02 23:20:16 +0300

    ilk katman eklendi

--- ilk_adim.py
+++ ilk_adim.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-# Sinir ağında her şey sayı dizisi (array)
+# --- TEK NÖRON ---
 girdi = np.array([1.5, 0.8, 2.1])    # 3 giriş özelliği
 agirlik = np.array([0.4, -0.2, 0.7]) # her özelliğin ağırlığı
 bias = 0.1
 
-# Nöronun yaptığı tek şey bu:
 sonuc = np.dot(girdi, agirlik) + bias
 print("Nöron çıktısı:", sonuc)
 
@@ -15,3 +14,19 @@ def relu(x):
 
 aktivasyon = relu(sonuc)
 print("Aktivasyon sonrası:", aktivasyon)
+
+# --- KATMAN (4 nöron aynı anda) ---
+katman_agirliklari = np.array([
+    [0.4, -0.2,  0.7],  # 1. nöron
+    [0.1,  0.5, -0.3],  # 2. nöron
+    [-0.6, 0.8,  0.2],  # 3. nöron
+    [0.9, -0.1,  0.4],  # 4. nöron
+])
+
+katman_bias = np.array([0.1, -0.2, 0.3, 0.0])
+
+katman_sonuc = np.dot(katman_agirliklari, girdi) + katman_bias
+print("Katman çıktısı:", katman_sonuc)
+
+katman_aktivasyon = np.maximum(0, katman_sonuc)
+print("Katman aktivasyon:", katman_aktivasyon)
